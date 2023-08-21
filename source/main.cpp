b0d7128a15e1c06b0387e84d56a69c2aeb14407e
MasaGratoR <masagrator@gmail.com>
2023-08-21 19:09:00 +0200

    Add support for nn::oe::GetDefaultDisplayResolution

--- source/main.cpp
+++ source/main.cpp
@@ -13,11 +13,13 @@ NsApplicationControlData appControlData;
 
 uint8_t Docked[0x10] = {0xE0, 0x03, 0x00, 0x32, 0xC0, 0x03, 0x5F, 0xD6, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 uint8_t Handheld[0x10] = {0x00, 0x00, 0xA0, 0x52, 0xC0, 0x03, 0x5F, 0xD6, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t DockedDefaultResolution[] = {0x02, 0xF0, 0x80, 0x52, 0x03, 0x87, 0x80, 0x52, 0x02, 0x00, 0x00, 0xB9, 0x23, 0x00, 0x00, 0xB9, 0x00, 0x00, 0x80, 0xD2, 0xC0, 0x03, 0x5F, 0xD6};
+uint8_t HandheldDefaultResolution[] = {0x02, 0xA0, 0x80, 0x52, 0x03, 0x5A, 0x80, 0x52, 0x02, 0x00, 0x00, 0xB9, 0x23, 0x00, 0x00, 0xB9, 0x00, 0x00, 0x80, 0xD2, 0xC0, 0x03, 0x5F, 0xD6};
 bool dockedflag = false;
 bool handheldflag = false;
 bool isAlbum = false;
-char Files[2][38] = { "_ZN2nn2oe18GetPerformanceModeEv.asm64", "_ZN2nn2oe16GetOperationModeEv.asm64" };
-char ReverseNX[128];
+char Files[3][51] = { "_ZN2nn2oe18GetPerformanceModeEv.asm64", "_ZN2nn2oe16GetOperationModeEv.asm64", "_ZN2nn2oe27GetDefaultDisplayResolutionEPiS1_.asm64"};
+char ReverseNX[192];
 uint8_t filebuffer[0x10] = {0};
 uint32_t countGames = 0;
 Flag changeFlag = Flag_Handheld;
@@ -44,11 +46,11 @@ bool isAllUpper(const std::string& word)
 }
 
 void RemoveReverseNX(u64 tid) {
-	if (tid == UINT64_MAX) for (uint8_t i = 0; i < 2; i++) {
+	if (tid == UINT64_MAX) for (uint8_t i = 0; i < 3; i++) {
 		snprintf(ReverseNX, sizeof ReverseNX, "sdmc:/SaltySD/patches/%s", Files[i]);
 		remove(ReverseNX);
 	}
-	else for (uint8_t i = 0; i < 2; i++) {
+	else for (uint8_t i = 0; i < 3; i++) {
 		snprintf(ReverseNX, sizeof ReverseNX, "sdmc:/SaltySD/patches/%016" PRIx64 "/%s", tid, Files[i]);
 		remove(ReverseNX);
 	}
@@ -103,6 +105,52 @@ void setReverseNX(uint64_t tid, Flag changedFlag) {
 					break;
 					
 			}
+		}
+	}
+	if (tid == UINT64_MAX) {
+		snprintf(ReverseNX, sizeof ReverseNX, "sdmc:/SaltySD/patches/%s", Files[2]);
+		FILE* asem = fopen(ReverseNX, "wb");
+		switch(changedFlag) {
+			case Flag_Handheld:
+				fwrite(HandheldDefaultResolution, 1, sizeof(HandheldDefaultResolution), asem);
+				fclose(asem);
+				break;
+			
+			case Flag_Docked:
+				fwrite(DockedDefaultResolution, 1, sizeof(DockedDefaultResolution), asem);
+				fclose(asem);
+				break;
+			
+			default:
+				fclose(asem);
+				remove(ReverseNX);
+				break;
+				
+		}
+	}
+	else {
+		snprintf(ReverseNX, sizeof ReverseNX, "sdmc:/SaltySD/patches/%016" PRIx64, tid);
+		DIR* patchdir = opendir(ReverseNX);
+		if (patchdir == NULL) mkdir(ReverseNX, 777);
+		else closedir(patchdir);
+		snprintf(ReverseNX, sizeof ReverseNX, "sdmc:/SaltySD/patches/%016" PRIx64 "/%s", tid, Files[2]);
+		FILE* asem = fopen(ReverseNX, "wb");
+		switch(changedFlag) {
+			case Flag_Handheld:
+				fwrite(HandheldDefaultResolution, 1, sizeof(HandheldDefaultResolution), asem);
+				fclose(asem);
+				break;
+			
+			case Flag_Docked:
+				fwrite(DockedDefaultResolution, 1, sizeof(DockedDefaultResolution), asem);
+				fclose(asem);
+				break;
+			
+			default:
+				fclose(asem);
+				remove(ReverseNX);
+				break;
+				
 		}
 	}
 }
